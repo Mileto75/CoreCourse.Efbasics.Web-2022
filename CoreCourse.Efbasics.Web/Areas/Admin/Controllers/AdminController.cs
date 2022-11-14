3d4047e8586f6344334933221839424c24758cfd
Mileto Di Marco <mileto.di.marco@howest.be>
2022-11-14 11:27:58 +0100

    Add admin homepage

---
+++ CoreCourse.Efbasics.Web/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreCourse.Efbasics.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.PageTitle = "Admin page";
+            return View();
+        }
+    }
+}
