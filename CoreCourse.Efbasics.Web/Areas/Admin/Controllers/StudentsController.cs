c42a0a198c3d915f1cc223d64ac064fac5dd0714
Mileto Di Marco <mileto.di.marco@howest.be>
2022-11-15 15:20:42 +0100

    Finish update functionality

--- CoreCourse.Efbasics.Web/Areas/Admin/Controllers/StudentsController.cs
+++ CoreCourse.Efbasics.Web/Areas/Admin/Controllers/StudentsController.cs
@@ -4,6 +4,7 @@ using CoreCourse.Efbasics.Web.Areas.Admin.ViewModels;
 using CoreCourse.Efbasics.Web.Data;
 using CoreCourse.Efbasics.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Constraints;
 using Microsoft.EntityFrameworkCore;
 using System.Data.Common;
 
@@ -185,12 +186,86 @@ namespace CoreCourse.Efbasics.Web.Areas.Admin.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Update(StudentsUpdateViewModel studentsAddViewModel)
+        public async Task<IActionResult> Update(StudentsUpdateViewModel studentsUpdateViewModel)
         {
             //get the student data
-            return View();
+            var student = await _schoolDbContext
+                .Students
+                .Include(s => s.Courses)
+                .FirstOrDefaultAsync(s => s.Id == studentsUpdateViewModel.Id);
+            //null check
+            if (student == null)
+            {
+                return NotFound();
+            }
+            //fill values
+            student.Firstname = studentsUpdateViewModel.Firstname;
+            student.Lastname = studentsUpdateViewModel.Lastname;
+            student.Username = studentsUpdateViewModel.Username;
+            //courses
+            //clear the courses
+            student.Courses.Clear();
+            //get the selected courses
+            var selectedCourses = studentsUpdateViewModel
+                .Courses
+                .Where(c => c.IsSelected == true)
+                .Select(c => c.Value);
+            //fill the student's courses
+            student.Courses = await
+                _schoolDbContext
+                .Courses
+                .Where(c => selectedCourses.Contains(c.Id)).ToListAsync();
+            //handle image
+            //check for image upload
+            if (studentsUpdateViewModel.Image != null)
+            {
+                //delete old image
+                var pathToDelete = Path
+                    .Combine(_webHostEnvironment.WebRootPath,
+                    "Images", student.Image);
+                //delete it
+                try
+                {
+                    System.IO.File.Delete(pathToDelete);
+                }
+                catch (FileNotFoundException fileNotFoundException)
+                {
+                    Console.WriteLine(fileNotFoundException.Message);
+                }
+                //create new filename
+                var newFileName = $"{Guid.NewGuid()}_{studentsUpdateViewModel.Image.FileName}";
+                //create Filestream and copy file
+                var pathToFile = Path
+                    .Combine(_webHostEnvironment.WebRootPath,
+                    "Images", newFileName);
+                using (FileStream fileStream = new FileStream(pathToFile,FileMode.Create))
+                {
+                    try
+                    {
+                        await studentsUpdateViewModel
+                            .Image.CopyToAsync(fileStream);
+                        
+                    }catch(Exception exception)
+                    {
+                        Console.WriteLine(exception.Message);
+                    }
+                    //add new filename to student
+                    student.Image = newFileName;
+                };
+            }
+            //store the student
+            try 
+            {
+                await _schoolDbContext.SaveChangesAsync();
+            }catch(DbException dbException) 
+            {
+                Console.WriteLine (dbException.Message);
+            }
+            //redirect to update using anonymous object to send id
+            return RedirectToAction("Update", new { Id = student.Id });
         }
 
     }
-    
 }
+
+
